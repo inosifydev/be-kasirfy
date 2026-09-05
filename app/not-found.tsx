ab465922c05f14be8a0e94521e5687bc020023aa
ademlna <ademaulanahidayah3103@gmail.com>
2026-09-05 23:03:16 +0700

    push

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFoundPage() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>404 - Halaman tidak ditemukan</h1>
-      <p>URL yang Anda tuju tidak tersedia.</p>
-    </main>
-  );
-}
